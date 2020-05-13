967722c1cad32371c9167d0fdd205ec43e1b7426
Anthony <agarciam@eafit.edu.co>
2020-05-13 16:52:44 -0500

    Taller 3

--- Ultrasonic/views.py
+++ Ultrasonic/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render, HttpResponse
 import requests
 
 def Ultrasonic(request):
-    # Verifica si hay un parámetro value en la petición GET
-    if 'value' in request.GET:
-        value = request.GET['value']
-        # Verifica si el value no esta vacio
-        if value:
+    # Verifica si hay un parámetro distance en la petición GET
+    if 'distance' in request.GET:
+        distance = request.GET['distance']
+        lat = request.GET['lat']
+        lng = request.GET['lng']
+        area = request.GET['area']
+        # Verifica si el distance no esta vacio
+        if distance:
             # Crea el json para realizar la petición POST al Web Service
-            args = {'type': 'ph', 'value': value}
+            args = {'distance': distance, 'lat': lat, 'lng': lng, 'area':area}
             response = requests.post('http://127.0.0.1:8000/ultrasonics/', args)
             # Convierte la respuesta en JSON
             Ultrasonic_json = response.json()
